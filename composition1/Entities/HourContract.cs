e0e738a4acb216b0c17037909dc6985c0ab62dd1
mat86 <mat86@MATHEUZINPC>
2023-04-27 20:09:46 -0300

    Add project files.

---
+++ composition1/Entities/HourContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace composition1.Entities
+{
+    internal class HourContract
+    {
+        public DateTime date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            this.date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
